1ef7f0c2a887a69885eba20da3f27be789edc1b0
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-18 16:42:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,4 @@
-class CashRegister :total, :add_item, :apply_discount, :items, 
+class CashRegister :total, :add_item, :apply_discount, :items, :void_last_transaction
   
   def initialize(age = 0)
     @age = age 
