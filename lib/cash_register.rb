57974d4e3711f4ebd0935d0133280570507f4d74
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-18 17:03:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,11 +11,11 @@ class CashRegister
 
 
    def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
+    if quantity > 1
+      count=0
+      while count < quantity
         @items << title
-        i+=1
+        count +=1
       end
     else
       @items << title
